2fae41b6a0ee2f918b09ce3d0e7ac1d3a42d0e98
prem19_wq <prabhuprem753@gmail.com>
2025-06-06 15:10:18 +0530

    rock

---
+++ script.js
@@ -0,0 +1,15 @@
+let i=0
+do{
+    i++;
+    console.log(i);
+}while(i<5)
+function greet(name ,age)
+{
+    return `hello ${name} is ${age} old`
+}
+console.log(greet('prem',18));
+function add(a,b)
+{
+    return a+b
+}
+console.log(add(2,3));
